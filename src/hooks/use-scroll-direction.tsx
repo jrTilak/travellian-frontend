134082e26f09faddbcef21ea686d9404114952a5
Tilak Thapa <iamjrtilak@gmail.com>
2024-02-07 22:08:13 +0545

    Refactor useScrollDirection hook to include height parameter

--- src/hooks/use-scroll-direction.tsx
+++ src/hooks/use-scroll-direction.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const useScrollDirection = () => {
+const useScrollDirection = (height: number = 50) => {
   const [scrollDirection, setScrollDirection] = useState<null | boolean>(null);
   const [lastScrollTop, setLastScrollTop] = useState(0);
 
@@ -9,14 +9,16 @@ const useScrollDirection = () => {
       const st = window.scrollY || document.documentElement.scrollTop;
       if (st > lastScrollTop) {
         setScrollDirection(false); // downscroll
+      } else if (st > height) {
+        setScrollDirection(true); // upscroll only if above given height
       } else {
-        setScrollDirection(true); // upscroll
+        setScrollDirection(false); // always false if below given height
       }
       setLastScrollTop(st <= 0 ? 0 : st);
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollTop]);
+  }, [lastScrollTop, height]);
 
   return scrollDirection;
 };
